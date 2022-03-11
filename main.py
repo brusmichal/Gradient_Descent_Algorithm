0655f3b6fe6e3d5fcedef5d16cfecc4fe70e5da4
mbrus <brus.michal@outlook.com>
2022-03-11 04:04:24 +0100

    first working implementation

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import math
+import numdifftools as nd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def f1(x):
+    return x ** 4
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def grad_f1(x):
+    return 4 * x ** 3
+
+
+def f2(x):
+    return 1.5 - math.exp(-x[0] ** 2 - x[1] ** 2) - 0.5 * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)
+
+
+def grad_f2(x):
+    return np.array([[2 * x[0] * math.exp(-x[0] ** 2 - x[1] ** 2) +
+                      (x[0] - 1) * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)],
+                     [2 * x[1] * math.exp(-x[0] ** 2 - x[1] ** 2) +
+                      (x[1] + 2) * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)]
+                     ])
+
+
+def gradient_descent(grad_func, start_point, learn_rate, max_iter, accuracy):
+    steps = np.array([start_point])
+    x = start_point
+    for _ in range(max_iter):
+        diff = grad_func(x)
+        if abs(x - diff) < accuracy:
+            break
+        x = x - learn_rate * diff
+        steps = np.append(steps, x)
+    return steps, x
+
+
+x_steps, final_result = gradient_descent(grad_f1, 8, 0.007, 100, 0.01)
+
+x_points = np.arange(-10, 10, 0.1)
+y_points = f1(x_points)
+y_steps = f1(x_steps)
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Function f1 with gradient descent steps")
+plt.plot(x_points, y_points)
+plt.plot(x_steps, y_steps)
+print(x_steps, final_result)
+plt.show()
