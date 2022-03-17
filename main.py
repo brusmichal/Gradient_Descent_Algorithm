eaa7b926234c60161395b4ae4baf11e6fd1279e2
mbrus <brus.michal@outlook.com>
2022-03-17 16:18:33 +0100

    changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-import numdifftools as nd
-import matplotlib.pyplot as plt
 import numpy as np
+import autograd as ag
+import math
+import matplotlib.pyplot as plt
 
 
 def f1(x):
@@ -13,39 +13,75 @@ def grad_f1(x):
 
 
 def f2(x):
-    return 1.5 - math.exp(-x[0] ** 2 - x[1] ** 2) - 0.5 * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)
+    e = math.e
+    return 1.5 - (e ** ((-x[0]) ** 2 - x[1] ** 2)) - 0.5 * (e ** ((-(x[0] - 1)) ** 2 - (x[1] + 2) ** 2))
 
 
 def grad_f2(x):
-    return np.array([[2 * x[0] * math.exp(-x[0] ** 2 - x[1] ** 2) +
-                      (x[0] - 1) * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)],
-                     [2 * x[1] * math.exp(-x[0] ** 2 - x[1] ** 2) +
-                      (x[1] + 2) * math.exp(-(x[0] - 1) ** 2 - (x[1] + 2) ** 2)]
+    x1 = x[0]
+    x2 = x[1]
+    return np.array([2 * x1 * np.exp(-x1 ** 2 - x2 ** 2) + (x1 - 1) * np.exp(-(x1 - 1) ** 2 - (x2 + 2) ** 2),
+                     2 * x2 * np.exp(-x1 ** 2 - x2 ** 2) + (x2 + 2) * np.exp(-(x1 - 1) ** 2 - (x2 + 2) ** 2)
                      ])
 
 
-def gradient_descent(grad_func, start_point, learn_rate, max_iter, accuracy):
-    steps = np.array([start_point])
-    x = start_point
+def gradient_descent(function, dimensions, max_iter, learn_rate=0.00007):
+
+    upper_bound = 100
+    x = np.random.uniform(-upper_bound, upper_bound, dimensions)
+    points_history = np.array([x])
     for _ in range(max_iter):
-        diff = grad_func(x)
-        if abs(x - diff) < accuracy:
-            break
+        diff = grad_f2(x)
         x = x - learn_rate * diff
-        steps = np.append(steps, x)
-    return steps, x
+        x[x < -upper_bound] = -upper_bound
+        x[x > upper_bound] = upper_bound
+        points_history = np.append(points_history, [x], axis=0)
+
+    optimum = np.array([x, function(x)])
+    return optimum, points_history
 
 
-x_steps, final_result = gradient_descent(grad_f1, 8, 0.007, 100, 0.01)
+# x_steps, final_result = gradient_descent(grad_f1, 8, 0.00391, 1000, 0.01)
+#
+# x_points = np.arange(-10, 10, 0.5)
+# y_points = f1(x_points)
+# y_steps = f1(x_steps)
+#
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("Function f1 with gradient descent steps")
+# plt.plot(x_points, y_points)
+# plt.plot(x_steps, y_steps)
+# print(x_steps, final_result)
+# plt.show()
 
-x_points = np.arange(-10, 10, 0.1)
-y_points = f1(x_points)
-y_steps = f1(x_steps)
+# uruchomienie algorytmu
 
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Function f1 with gradient descent steps")
-plt.plot(x_points, y_points)
-plt.plot(x_steps, y_steps)
-print(x_steps, final_result)
-plt.show()
+
+# wykres poziomicowy
+# MAX_X = 100
+# PLOT_STEP = 0.1
+#
+# x_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+# y_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+# X, Y = np.meshgrid(x_arr, y_arr)
+# Z = np.empty(X.shape)
+# func = f2
+#
+# for i in range(X.shape[0]):
+#     for j in range(X.shape[1]):
+#         Z[i, j] = func(np.array([X[i, j], Y[i, j]]))
+#
+# plt.contour(X, Y, Z, 20)
+#
+# # uruchomienie algorytmu
+result, steps = gradient_descent(f2, 2, 100)
+print(f"Optimum w punkcie: {result[0]} wynosi {result[1]}.")
+print("Kroki:", steps)
+#
+# for i in range(len(steps) - 1):
+#     plt.arrow(steps[i], func(steps[i]), steps[i + 1] - steps[i], func(steps[i + 1]) - func(steps[i]), head_width=3,
+#               head_length=6, fc='k', ec='k')
+#
+# plt.show()
+#
